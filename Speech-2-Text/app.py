172f4cdc9bba9c2d66267cf2d6c702d82cd9cc57
thibaultconte <thibault1.conte@epitech.eu>
2023-09-06 09:58:50 +0200

    [:hammer: | :zap:] Refonte et amélioration du système de speech to text via socket.io

--- Speech-2-Text/app.py
+++ Speech-2-Text/app.py
@@ -4,34 +4,37 @@ import speech_recognition as sr
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-# Initialize the Recognizer instance
 recognizer = sr.Recognizer()
 
 @socketio.on('start_recording')
 def start_recording():
     with sr.Microphone() as source:
-        socketio.emit("status_recording", {'status': "Enregistrement traduit avec succés"})
         while True:
             try:
+                socketio.emit("status_recording", {'status': "Enregistrement en cours ..."})
                 recognizer.adjust_for_ambient_noise(source=source)
-                text = recognizer.recognize_google(recognizer.listen(source), language="fr-FR")
+                text = recognizer.recognize_google(recognizer.listen(source, timeout=3, phrase_time_limit=5), language="fr-FR")
                 socketio.emit('recognized_text', {'text': text})
-                print(text)
-                if text:
-                    socketio.emit("status_recording", {'status': "Enregistrement traduit avec succés"})
+                socketio.emit("status_recording", {'status': "Enregistrement transcrit avec succés."})
+                break
             except sr.WaitTimeoutError:
+                socketio.emit("error_recording", {'status': "Aucune voix détecté, vueillez réessayer."})
                 break
             except sr.UnknownValueError:
-                socketio.emit({"status_recording": "Impossible, je ne comprend pas ce que vous dites"})
+                socketio.emit("error_recording", {'status': "Impossible, je ne comprend pas ce que vous dites."})
+                break
+            except sr.RequestError as error:
+                socketio.emit ("error_recording", {'status': f"Impossible de contacter Google pour l'instant, vueillez réessayer plus tard : {error}"})
+                break
 
 @socketio.on('stop_recording')
 def start_recording():
     socketio.emit("status_recording", {'status': "Enregistrement stoppé avec succés"})
+    return False
 
 @app.route('/')
 def index():
-    return render_template('test.html')
+    return render_template('speech_to_text_test.html')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- Speech-2-Text/speech-to-text.py
+++ Speech-2-Text/speech-to-text.py
@@ -6,10 +6,14 @@ with sr.Microphone() as source:
     while True:
         try:
             recognizer.adjust_for_ambient_noise(source=source)
-            audio_data = recognizer.listen(source, timeout=1)
+            audio_data = recognizer.listen(source, timeout=5)
             print(audio_data)
             text = recognizer.recognize_google(audio_data, language="fr-FR")
             print(text)
         except sr.WaitTimeoutError:
+            print("je passe dans le timeout")
+            break
+        except sr.UnknownValueError:
+            print("La valeur est inconnue")
             break
     
